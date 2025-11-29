4cce39792fca51692d40d123357bc9abaa6ef174
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-11-29 18:06:34 +0600

    hero component design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
+import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <>
       <Navbar />
+      <main className="max-w-6xl mx-auto px-4 py-12 space-y-10">
+        <Hero />
+      </main>
     </>
   );
 }
