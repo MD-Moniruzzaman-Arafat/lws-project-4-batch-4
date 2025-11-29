fe708e9a291360c8c63e40a9754bc3f4bc2d246f
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-11-29 18:11:04 +0600

    sidebar component design

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
+import SideBar from './components/SideBar/SideBar';
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
       <Navbar />
       <main className="max-w-6xl mx-auto px-4 py-12 space-y-10">
         <Hero />
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+          <SideBar />
+        </div>
       </main>
     </>
   );
